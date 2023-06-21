6c0dc69d03300dd3077234d6bfa443db69a58ac2
temerk1n <nikita.verigin23@gmail.com>
2023-06-21 18:51:04 +0300

    feat: showcase

--- ticket_poisk/src/app/page.tsx
+++ ticket_poisk/src/app/page.tsx
@@ -1,3 +1,15 @@
+"use client"
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import TicketCard from "@/components/TicketCard/TicketCard";
+
 export default function Home() {
-  return null;
+
+  return (
+    <>
+      <Header />
+      <TicketCard title="Властелин колец: Братство Кольца" genre="Фэнтези" posterUrl="https://i.postimg.cc/pdCLNMqX/1.webp" />
+      <Footer />
+    </>
+  );
 }
