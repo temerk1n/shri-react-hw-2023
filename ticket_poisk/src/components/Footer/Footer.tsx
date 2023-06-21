cab50331d5ab7e008b69a6768d9a4ca150fe6496
temerk1n <nikita.verigin23@gmail.com>
2023-06-21 18:50:10 +0300

    feat: added footer

---
+++ ticket_poisk/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css'
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <div className={ styles.footer }>
+            <Link href='/qa'>Вопросы-ответы</Link>
+            <Link href='/about'>О нас</Link>
+        </div>
+    )
+}
