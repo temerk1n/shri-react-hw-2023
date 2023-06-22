d2e8dc68f3f757a0d2a94c3d50bf63cdc9f67b77
temerk1n <nikita.verigin23@gmail.com>
2023-06-22 13:40:01 +0300

    feat: added TicketCardList

---
+++ ticket_poisk/src/components/TicketCardList/TicketCardList.tsx
@@ -0,0 +1,21 @@
+import Movie from "@/entities/Movie";
+import TicketCard from "@/components/TicketCard/TicketCard";
+import styles from './TicketCardList.module.css'
+
+interface TicketCardListProps {
+  movies: Movie[],
+}
+
+export default function TicketCardList({movies}: TicketCardListProps) {
+
+  const tickets = movies.map((movie) => {
+    return <TicketCard title={movie.title} genre={movie.genre} posterUrl={movie.posterUrl} key={movie.id}/>
+  })
+
+
+  return (
+    <div className={styles.container}>
+      { tickets }
+    </div>
+  )
+}
