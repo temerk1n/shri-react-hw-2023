d548825d0ed02ab3104f29c0c5fb795cfb910f63
temerk1n <nikita.verigin23@gmail.com>
2023-06-22 13:37:32 +0300

    fix: Review is now a class

--- ticket_poisk/src/entities/Review.ts
+++ ticket_poisk/src/entities/Review.ts
@@ -1,7 +1,21 @@
-export default interface Review {
-    id: string;
-    name: string;
-    text: string;
-    rating: number;
+interface ReviewProps {
+    id: string,
+    name: string,
+    text: string,
+    rating: number,
+}
+
+export default class Review {
+    public readonly id: string;
+    public readonly name: string;
+    public readonly text: string;
+    public readonly rating: number;
+
+    constructor({id, name, text, rating}: ReviewProps) {
+        this.id = id;
+        this.name = name;
+        this.text = text;
+        this.rating = rating;
+    }
 
 }
