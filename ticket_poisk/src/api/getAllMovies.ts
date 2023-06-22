fa5c30328a09e4b6aa0dd3fcf18860127ca3dd2c
temerk1n <nikita.verigin23@gmail.com>
2023-06-22 13:38:39 +0300

    fix: requests to api

--- ticket_poisk/src/api/getAllMovies.ts
+++ ticket_poisk/src/api/getAllMovies.ts
@@ -1,7 +1,8 @@
 import httpClient from "@/api/httpClient";
+import Movie from "@/entities/Movie";
 
 const getAllMovies = () => httpClient
     .get('/movies')
-    .then((response) => response.data);
+    .then((response) => response.data.map((movie: Movie) => new Movie(movie)));
 
 export default getAllMovies;
