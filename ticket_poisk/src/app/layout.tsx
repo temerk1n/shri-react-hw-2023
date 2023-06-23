814a14a674f237d36e9e751e22e07d17e26ab5c5
temerk1n <nikita.verigin23@gmail.com>
2023-06-23 11:59:18 +0300

    chore: added global font

--- ticket_poisk/src/app/layout.tsx
+++ ticket_poisk/src/app/layout.tsx
@@ -3,6 +3,14 @@ import { Metadata } from "next";
 import styles from "./layout.module.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import {Roboto} from "next/font/google"
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: 'Билетопоиск',
@@ -16,14 +24,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body>
-      <div className={styles.page}>
-        <Header />
-        <div className={styles.content}>
-          {children}
+      <body className={ roboto.className }>
+        <div className={styles.page}>
+          <Header />
+          <div className={styles.content}>
+            {children}
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
       </body>
     </html>
   )
